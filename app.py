7083e21ecb1ad5ba1c26041fe1eba86564199338
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2020-10-23 10:44:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,10 @@ from linebot.models import (
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+return "Hello to chatbot web service"
+
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = '7f819199fc35d2461ceb0191d0fb304d'
 channel_access_token = 't8TS42nUWRlHempLf4OLMEf1xoNm96YHojEt71MgX96NGuA9qucXNT/4nJtBscYdXZt/ADJLVbqfcwIbdSrlqsW0s0z6i8GPPWtipaaGnOoj0UhNrGI7eeOXAzRf4A6s1hdq+CraBNPxexpYI3TwowdB04t89/1O/w1cDnyilFU='
